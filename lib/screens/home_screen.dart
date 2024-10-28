0785edc08a6f7b6986020c37d1692c633ddda976
shikhar <shikhars369@gmail.com>
2024-10-28 23:51:26 +0530

    end

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:printing/printing.dart';
 import 'package:resume/resume_pdf.dart'; // For PdfPreview widget
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
